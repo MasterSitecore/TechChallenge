3bb9e5d01bd3f0c5eeebcf12b8a3a259660771e2
MasterSitecore <45512586+MasterSitecore@users.noreply.github.com>
2019-06-15 08:42:28 +0530

    Add files via upload

---
+++ TechChallenge.Project/Services/ResponseDataService.cs
@@ -0,0 +1,28 @@
+﻿using TechChallenge.Project.Interfaces;
+using TechChallenge.Project.Models;
+
+namespace TechChallenge.Project.Services
+{
+    public class ResponseDataService : IResponseDataService
+    {
+        private ResponseDataModel responseMessage;
+
+        public ResponseDataService()
+        {
+            responseMessage = new ResponseDataModel();
+        }
+
+        public ResponseDataModel BadRequestErrorMessage(string message)
+        {
+            responseMessage.Message = message;
+            responseMessage.StatusCode = (int)StatusCode.BadRequest;
+
+            return responseMessage;
+        }
+
+        private enum StatusCode
+        {
+            BadRequest = 400,
+        }
+    }
+}
